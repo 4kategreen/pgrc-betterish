2790c65c77a14b90bfdae30d7165b2692da55f88
4kategreen <kate@kategreen.us>
2013-06-01 14:23:45 -0400

    configuring Results

--- app/scripts/services/Results.js
+++ app/scripts/services/Results.js
@@ -3,6 +3,6 @@
 angular.module('pgrcApp')
   .factory('Results', function (Parse) {
     var Results = angular.copy(Parse.Model);
-    Results.configure('results');
+    Results.configure('results','age','bib','chip','city','cnipTime','divisionName','firstName','gender','lastName','pace','place','race','state');
     return Races;
   });
