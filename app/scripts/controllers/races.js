e49f35c8795387b58712b88269f796ddfe2dcbff
Kate Green <kate.green@chronicle.com>
2013-06-05 14:38:47 -0400

    Adding some tests to the races controller.

--- app/scripts/controllers/races.js
+++ app/scripts/controllers/races.js
@@ -3,40 +3,50 @@
 angular.module('pgrcApp')
   .controller('RacesCtrl', ['$scope', 'angularFire', function ($scope, angularFire) {
     var url = 'https://kategreen.firebaseio.com/races';
-    var promise = angularFire(url, $scope, 'races');
+    var promise = angularFire(url, $scope, 'races', []);
 
     promise.then(function() {
-      var today = new Date();
-      angular.forEach($scope.races, function(race,key) {
-        var pastRegDate = today > new Date(race.registrationCloseDate) ? true : false;
-        var pastDate = today > new Date(race.date) ? true : false;
-        var results = race.results ? race.results.length : 0;
-        $scope.races[key].state = race.state || 'closed';
-
-        if (race.registrationCloseDate) {
-          if (pastRegDate) {
-            if (results > 0) {
-              $scope.races[key].state = 'results';
-            } else {
-              $scope.races[key].state = 'closed';
-            }
-          } else {
-            $scope.races[key].state = 'open';
-          }
-        } else {
-          if (pastDate) {
-            if (results > 0) {
-              $scope.races[key].state = 'results';
-            } else {
-              $scope.races[key].state = 'closed';
-            }
-          }
-        }
-      });
+      $scope.setState();
       // Or, attach a function to $scope that will let a directive in markup manipulate the model.
       $scope.removeItem = function() {
         $scope.items.splice($scope.toRemove, 1);
         $scope.toRemove = null;
       };
     });
+
+    $scope.setState = function() {
+      var today = new Date();
+      $scope.races.forEach(function(race,key) {
+        $scope.races[key].id = key;
+        var pastRegDate = today > new Date(race.registrationCloseDate) ? true : false;
+        var pastDate = today > new Date(race.date) ? true : false;
+        var results = race.results ? race.results.length : 0;
+        var state = 'closed';
+
+        if (race.registrationCloseDate) {
+          if (pastRegDate) {
+            if (results > 0) {
+              state = 'results';
+            } else {
+              state = 'closed';
+            }
+          } else {
+            state = 'open';
+          }
+        } else {
+          if (pastDate) {
+            if (results > 0) {
+              state = 'results';
+            } else {
+              state = 'closed';
+            }
+          } else {
+            state = 'open';
+          }
+        }
+        console.log(state);
+        $scope.races[key].state = state;
+        console.log($scope.$root.$$phase);
+      });
+    };
   }]);
--- test/spec/controllers/races.js
+++ test/spec/controllers/races.js
@@ -15,6 +15,43 @@ describe('Controller: RacesCtrl', function () {
       $scope: scope
     });
     scope.today = new Date('2013-06-01');
+    scope.races = [
+      {
+        "name": "Laurel Pregnancy Center",
+        "distance": "5K",
+        "date": "2013-04-21",
+        "registration": "www.google.com",
+        "registrationCloseDate": "2013-04-18",
+        "results": [
+          {
+            "firstName": "Kate",
+            "lastName": "Green",
+            "gender": "F",
+            "time": "30:00"
+          },
+          {
+            "firstName": "Kim",
+            "lastName": "Ossi",
+            "gender": "F",
+            "time": "31:00"
+          }
+        ]
+      },
+      {
+        "name": "Tortoise and the Hare",
+        "distance": "5K",
+        "date": "2013-04-28",
+      },
+      {
+        "name": "Beltsville Academy",
+        "distance": "5K",
+        "date": "2013-06-08",
+        "registration": "www.google.com",
+        "registrationCloseDate": "2013-06-06"
+      }
+    ];
+
+    scope.setState();
   }));
 
   // it('should attach a list of awesomeThings to the scope', function () {
@@ -22,6 +59,8 @@ describe('Controller: RacesCtrl', function () {
   // });
 
   it('should determine the state of a race correctly so it displays things right', function() {
-
+    expect(scope.races[0].state).toBe('results');
+    expect(scope.races[1].state).toBe('closed');
+    expect(scope.races[2].state).toBe('open');
   });
 });
