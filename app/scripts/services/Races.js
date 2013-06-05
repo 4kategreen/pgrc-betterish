245c83bf4772a3a7cce83401ea78a51fa4863bad
Kate Green <kate.green@chronicle.com>
2013-06-05 14:39:02 -0400

    Removing some stuff I'm not using.

--- app/scripts/services/Races.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('pgrcApp')
-  .factory('Races', function (Parse) {
-    var Races = angular.copy(Parse.Model);
-    Races.configure('races','cityState','clubRace','date','distance','location','name','registrationLink','results');
-    return Races;
-  });
